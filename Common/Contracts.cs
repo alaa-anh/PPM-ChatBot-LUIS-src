38c34ce5bb5924488a9221bf684fc77af6679373
alaa-anh <36130341+alaa-anh@users.noreply.github.com>
2018-02-07 11:43:01 +0200

    Mongo DB version retrived

--- Common/Contracts.cs
+++ Common/Contracts.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿//using MongoDB.Bson;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+using System;
 
 namespace Common.Contracts
 {
