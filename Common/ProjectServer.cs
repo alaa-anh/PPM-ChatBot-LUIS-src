9b2724052d1366bb5e83ba7257e2549c24e13399
alaa-anh <36130341+alaa-anh@users.noreply.github.com>
2018-03-28 08:24:28 +0200

    error fix

--- Common/ProjectServer.cs
+++ Common/ProjectServer.cs
@@ -270,15 +270,15 @@ namespace Common
                     // cardButtons.Add(CardButton);
 
 
-           //         List<CardImage> cardImages = new List<CardImage>();
+                    List<CardImage> cardImages = new List<CardImage>();
 
-                  //  cardImages.Add(new CardImage(url: "https://ppm-chatbot-luis.scm.azurewebsites.net/dev/wwwroot/Images/"+ CurrentNumber + ".jpg"));
+                    cardImages.Add(new CardImage(url: "https://ppm-chatbot-luis.scm.azurewebsites.net/dev/wwwroot/Images/" + CurrentNumber + ".jpg"));
 
 
                     ThumbnailCard plCardCounter = new ThumbnailCard()
                     {
                          Title = "Page " + CurrentNumber,
-                       // Images = cardImages,
+                        Images = cardImages,
                         Tap = CardButton,
 
                     };
