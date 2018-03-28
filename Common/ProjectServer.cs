0c5c1d45edd58a2cbddd625e98dd746bcfda5dc5
alaa-anh <36130341+alaa-anh@users.noreply.github.com>
2018-03-28 08:32:18 +0200

    paging error

--- Common/ProjectServer.cs
+++ Common/ProjectServer.cs
@@ -155,7 +155,7 @@ namespace Common
 
                     if (context.Projects.Count >= 10)
                     {
-                     //   List<CardAction> cardButtons = CreateButtonsProjectsPaging(context.Projects.Count);
+                        List<CardAction> cardButtons = CreateButtonsProjectsPaging(context.Projects.Count);
                         string subTitle = string.Empty;
                         if (SIndex == 0)
                             subTitle = "You are viwing the first page , each page view 10 projects";
@@ -168,10 +168,10 @@ namespace Common
                         {
                             Title = "**Total Number Of availabel Projects :**\n" + projectDetails.Count,
                             Subtitle = subTitle,
-                      //      Buttons = cardButtons,
+                            Buttons = cardButtons,
 
                         };
-                        reply.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+                       // reply.AttachmentLayout = AttachmentLayoutTypes.Carousel;
                         reply.Attachments.Add(plCardCounter.ToAttachment());
                     }
                     else
@@ -198,41 +198,41 @@ namespace Common
         }
 
 
-        //public  List<CardAction> CreateButtonsProjectsPaging( int totalCount)
-        //{
-        //    List<CardAction> cardButtons = new List<CardAction>();
-        //    //double p = totalCount * 0.1;
-        //    //double result = Math.Ceiling(p);
-        //    //int pagenumber = int.Parse(result.ToString());
+        public List<CardAction> CreateButtonsProjectsPaging(int totalCount)
+        {
+            List<CardAction> cardButtons = new List<CardAction>();
+            double p = totalCount * 0.1;
+            double result = Math.Ceiling(p);
+            int pagenumber = int.Parse(result.ToString());
 
-        //    //string valuebutton = string.Empty;
-        //    //for (int i = 0; i < pagenumber; i++)
-        //    //{
-        //    //    string CurrentNumber = Convert.ToString(i);
-
-
-        //    //    if (i == 0)
-        //    //    {
-        //    //        valuebutton = "get all projects at index 0";
-        //    //    }
-        //    //    else
-        //    //        valuebutton = "get all projects at index " + i * 10;
-
-        //    //    CurrentNumber = Convert.ToString(i + 1);
-        //    //    CardAction CardButton = new CardAction()
-        //    //    {
-        //    //        Type = ActionTypes.PostBack,
-        //    //        Title = CurrentNumber,
-        //    //        Value = valuebutton,
-        //    //        Image = "http://www.kidsmathgamesonline.com/images/pictures/numbers600/number"+i+".jpg"
-        //    //    };
-        //    //    cardButtons.Add(CardButton);
+            string valuebutton = string.Empty;
+            for (int i = 0; i < pagenumber; i++)
+            {
+                string CurrentNumber = Convert.ToString(i);
 
 
+                if (i == 0)
+                {
+                    valuebutton = "get all projects at index 0";
+                }
+                else
+                    valuebutton = "get all projects at index " + i * 10;
 
-        //    //}
-        //    return cardButtons;
-        //}
+                CurrentNumber = Convert.ToString(i + 1);
+                CardAction CardButton = new CardAction()
+                {
+                    Type = ActionTypes.PostBack,
+                    Title = CurrentNumber,
+                    Value = valuebutton,
+                    Image = "http://www.kidsmathgamesonline.com/images/pictures/numbers600/number" + i + ".jpg"
+                };
+                cardButtons.Add(CardButton);
+
+
+
+                }
+                return cardButtons;
+        }
 
 
         public IMessageActivity CreateButtonsProjects(IDialogContext dialogContext , int totalCount)
