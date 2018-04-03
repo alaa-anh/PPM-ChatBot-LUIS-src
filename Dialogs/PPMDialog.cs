ec2f4d88a00db4385a2e368d864d2ee3cbfe884a
alaa-anh <36130341+alaa-anh@users.noreply.github.com>
2018-04-03 15:50:46 +0300

    ref error

--- Dialogs/PPMDialog.cs
+++ Dialogs/PPMDialog.cs
@@ -460,7 +460,7 @@ namespace Microsoft.Bot.Sample.LuisBot
             }
             else
             {
-                PromptDialog.Confirm(context, ResumeAfterConfirmation, userName + "--" + password + " --" + UserLoggedInName + "--" + "The User Don't have permission , do you want to try another cridentials?");
+                PromptDialog.Confirm(context, ResumeAfterConfirmation, "The User Don't have permission , do you want to try another cridentials?");
 
             }
         }
